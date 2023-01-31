529221e0b9b0c92823809c101a1d29217bb927f4
NamNguyen310801 <92281093+NamNguyen310801@users.noreply.github.com>
2023-01-31 12:37:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-p5odr4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-p5odr4)
---
+++ script.js
@@ -0,0 +1,20 @@
+var listCount = document.querySelectorAll('.counter');
+function count(el) {
+  var numberEl = el.querySelector('.number');
+  var to = parseInt(numberEl.innerText);
+  var count = 0;
+  var time = 150;
+  var step = to / time;
+  let counting = setInterval(() => {
+    count += step;
+    if (count > to) {
+      clearInterval(counting);
+      numberEl.innerText = to;
+    } else {
+      numberEl.innerText = Math.round(count);
+    }
+  });
+}
+listCount.forEach((item) => {
+  count(item);
+});
